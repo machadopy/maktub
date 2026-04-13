25a0b589b38f4efc24a1b16faac8f90cebc3e3ba
machado.py <vitorandroidcwb@gmail.com>
2026-04-13 08:23:20 -0300

    maktub

---
+++ JS/mobile.menu.js
@@ -0,0 +1,51 @@
+class MobileNavbar{
+    constructor(mobileMenu, navlist, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navlist);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        console.log(this)
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.navList.classList.toggle(this.activeClass);
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init(){
+        if (this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+    const mobileNavbar = new MobileNavbar(
+        ".menuham",
+        ".nav-list",
+        ".nav-list li",
+)
+
+const menu = document.querySelector(".menuham");
+
+menu.addEventListener("click", () => {
+    menu.classList.add("click-effect");
+
+    setTimeout(() => {
+        menu.classList.remove("click-effect");
+    }, 600);
+}); 
+
+mobileNavbar.init();
+
+const icon = document.getElementById("icon");
+
+icon.addEventListener("click", () => {
+  icon.classList.toggle("fi-rr-menu-burger");
+  icon.classList.toggle("fi-rr-cross-small");
+});
